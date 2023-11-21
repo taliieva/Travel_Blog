4dc3bce3eb7ca0d609907b1d27254f3cd8987362
taliieva <teranealieva01@gmail.com>
2023-11-21 19:19:45 +0400

    validation

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -6,7 +6,21 @@ import facebookLogo from "../../../public/assets/facebookFrame.svg";
 import eye from "../../../public/assets/01 align center.svg";
 import { Link } from "react-router-dom";
 import style from "./SignIn.module.css";
+import { Controller, FormProvider, useFormContext, useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 export default function SignIn() {
+  // const methods = useFormContext();
+  const formMethod = useForm({
+    mode: "all",
+    defaultValues: {
+      mail: "",
+      password: "",
+    },
+  });
+  const submitFunc = (value) => {
+      console.log(value)
+  }
+  
   return (
     <div className={style.signMain}>
       <div className={style.signPage}>
@@ -17,21 +31,48 @@ export default function SignIn() {
               <img src={signLogo} alt="" />
             </Link>
           </div>
+          {/* <FormProvider {...formMethod}> */}
           <div className={style.signFormSection}>
             <h2>Sign in for your exciting journey</h2>
-            <form action="#" className={style.signFormContainer}>
+            <form action="#" className={style.signFormContainer} onSubmit={formMethod.handleSubmit(submitFunc)}>
               <label htmlFor="mail">Email</label>
 
-              <input
+              <Controller
+              name = "mail"
+              control={formMethod.control}
+              rules={{
+                required:"This field must be written!",
+                validate: (value)=>{
+                  return(
+                    !value.includes("@" && ".com") && "You must write gmail!"
+                  )
+                }
+              }}
+              render={({field})=>(
+                <input
+                {...field}
                 type="text"
                 id="mail"
                 placeholder="someone@example.com"
                 className={style.mail}
               />
+              )}
+              />
+              {formMethod.formState.errors?.mail && (
+                <div className={style.form_error}>{formMethod.formState.errors?.mail?.message}</div>
+              )}
 
               <label htmlFor="password">Password</label>
-              <div className={style.passwordDiv}>
+              <Controller
+              name="password"
+              control={formMethod.control}
+              rules={{
+                required: "This field must be written!",
+              }}
+              render={({field})=>(
+                <div className={style.passwordDiv}>
                 <input
+                {...field}
                   type="password"
                   name=""
                   id="password"
@@ -39,12 +80,19 @@ export default function SignIn() {
                 />
                 <img src={eye} alt="" />
               </div>
+              )}
+              />
+              {formMethod.formState.errors?.password && (
+                <div className={style.form_error}>{formMethod.formState.errors?.password?.message}</div>
+              )}
               <Link className={style.forgotPassword} to="/ForgotPassword">
                 Forgot password?
               </Link>
               <button>Sign in</button>
             </form>
           </div>
+          <DevTool control={formMethod.control}/>
+          {/* </FormProvider> */}
           <div className={`${style.goLoginSection} ${style.topLoginSection}`}>
             <p>
               Don`t have an account?{" "}
